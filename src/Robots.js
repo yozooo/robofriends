e5dee974f159f3d125311f9d2fe9c2e00c1c7238
Josip Vojković <jvcmiljo87@gmail.com>
2023-06-29 20:47:31 +0200

    Add files via upload
    New updates and changes made

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yessenia.net',
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsah',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+    },
+    {
+        id: 5,
+        name: 'Chelsea Dietrich',
+        username: 'Kanren',
+        email: 'Lucio_Hettinger@annie.ca',
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo',
+        email: 'Karley_Dach@jasper.info',
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Erwin.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir',
+        username: 'Maxine_Nienow',
+        email: 'Sherwood@rosamond.ne',
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chain_Mcdernott@dana.io',
+    },
+    {
+        id: 10,
+        name: 'Clara Dubuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+    },
+]
